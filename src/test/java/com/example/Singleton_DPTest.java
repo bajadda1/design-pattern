22fc1bfd441de5079ce74e64b55d7e8f04347074
AHMED BAJADDA <ahmedbajadda@gmail.com>
2024-02-07 23:37:34 +0100

    add singleton design pattern class and its test class:Singleton_DP

--- src/test/java/com/example/Singleton_DPTest.java
+++ src/test/java/com/example/Singleton_DPTest.java
@@ -1,4 +1,15 @@
+package com.example;
+
 import junit.framework.TestCase;
-public class Singleton_DPTest extends TestCase {
-  
+import org.junit.Test;
+
+public class Singleton_DPTest  {
+    @Test
+    public void TestUnicity(){
+        Singleton_DP singleton1=Singleton_DP.getInstance();
+        Singleton_DP singleton2=Singleton_DP.getInstance();
+        TestCase.assertEquals(singleton2,singleton1);
+
+    }
+
 }
